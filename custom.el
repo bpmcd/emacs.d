42f6d02894d4d1c890fbcc6cd3317e2c7bb32caf
Bobby Calderwood <bobby@thinkrelevance.com>
2011-04-21 22:37:50 -0400

    Set hl-line face color to #212121.

--- custom.el
+++ custom.el
@@ -11,4 +11,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:inherit nil :background "#212121"))))
  '(magit-diff-add ((((class color) (background dark)) (:foreground "green")))))
